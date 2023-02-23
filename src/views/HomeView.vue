1ec5c43c9f3996eb98e3536183d984bff36e316e
saircode <sairjsanchez@gmail.com>
2023-02-23 11:04:33 -0500

    Tabla bootstrap inicializada para ir viendo como se ubican los dientes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,64 @@
 <script setup>
-
+  // import t11 from "@/assets/img/teeth/11-18/11/sano.png";
 </script>
 
 <template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card mt-2">
+          <div class="card-header">
+            <h2>Odontograma</h2>
+          </div>
+          <div class="card-body table-responsive">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">18</th>
+                  <th scope="col">17</th>
+                  <th scope="col">16</th>
+                  <th scope="col">15</th>
+                  <th scope="col">14</th>
+                  <th scope="col">13</th>
+                  <th scope="col">12</th>
+                  <th scope="col">11</th>
+                  
+                  <th scope="col">21</th>
+                  <th scope="col">22</th>
+                  <th scope="col">23</th>
+                  <th scope="col">24</th>
+                  <th scope="col">25</th>
+                  <th scope="col">26</th>
+                  <th scope="col">27</th>
+                  <th scope="col">28</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td><img src="@/assets/img/teeth/11-18/11/11.png" width="50" alt=""></td>
+                  <td><img src="@/assets/img/teeth/11-18/11/11.png" width="50" alt=""></td>
+                  <td><img src="@/assets/img/teeth/11-18/11/11.png" width="50" alt=""></td>
+                  <td><img src="@/assets/img/teeth/11-18/11/11.png" width="50" alt=""></td>
+                  <td><img src="@/assets/img/teeth/11-18/11/11.png" width="50" alt=""></td>
+                  <td><img src="@/assets/img/teeth/11-18/11/11.png" width="50" alt=""></td>
+                  <td><img src="@/assets/img/teeth/11-18/11/11.png" width="50" alt=""></td>
+                  <td><img src="@/assets/img/teeth/11-18/11/11.png" width="50" alt=""></td>
+                  <td><img src="@/assets/img/teeth/11-18/11/11.png" width="50" alt=""></td>
+                  <td><img src="@/assets/img/teeth/11-18/11/11.png" width="50" alt=""></td>
+                  <td><img src="@/assets/img/teeth/11-18/11/11.png" width="50" alt=""></td>
+                  <td><img src="@/assets/img/teeth/11-18/11/11.png" width="50" alt=""></td>
+                  <td><img src="@/assets/img/teeth/11-18/11/11.png" width="50" alt=""></td>
+                  <td><img src="@/assets/img/teeth/11-18/11/11.png" width="50" alt=""></td>
+                  <td><img src="@/assets/img/teeth/11-18/11/11.png" width="50" alt=""></td>
+                  <td><img src="@/assets/img/teeth/11-18/11/11.png" width="50" alt=""></td>
 
+                </tr>
+                
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
