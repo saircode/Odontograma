8f6da2d2b7f9a030bc7b61081303d7be07bd7f01
saircode <sairjsanchez@gmail.com>
2023-04-13 15:20:52 -0500

    se organizan los dientes desde vue y no desde html

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,3 @@
-<script setup>
-  // import t11 from "@/assets/img/teeth/11-18/11/sano.png";
-</script>
-
 <template>
   <div class="container">
     <div class="row">
@@ -37,47 +33,28 @@
               </thead>
               <tbody>
                 <tr>
-                  <td class="text-center"><img src="@/assets/img/teeth/11-18/18/18.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/11-18/17/17.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/11-18/16/16.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/11-18/15/15.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/11-18/14/14.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/11-18/13/13.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/11-18/12/12.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/11-18/11/11.png" width="60" alt=""></td>
+                  <td v-for="(item, key) in teeth[0]" :key="key" class="text-center">
+                    <img :src="item.img" width="60" alt="">
+                  </td>
 
                   <td class="text-center"></td>
 
-                  <td class="text-center"><img src="@/assets/img/teeth/21-28/21/21.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/21-28/22/22.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/21-28/23/23.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/21-28/24/24.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/21-28/25/25.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/21-28/26/26.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/21-28/27/27.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/21-28/28/28.png" width="60" alt=""></td>
+                  <td v-for="(item, key) in teeth[1]" :key="key" class="text-center">
+                    <img :src="item.img" width="60" alt="">
+                  </td>
+                
                 </tr>
 
                 <tr>
-                  <td class="text-center"><img src="@/assets/img/teeth/41-48/48/48.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/41-48/47/47.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/41-48/46/46.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/41-48/45/45.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/41-48/44/44.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/41-48/43/43.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/41-48/42/42.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/41-48/41/41.png" width="60" alt=""></td>
+                  <td v-for="(item, key) in teeth[2]" :key="key" class="text-center">
+                    <img :src="item.img" width="60" alt="">
+                  </td>
 
                   <td class="text-center"></td>
 
-                  <td class="text-center"><img src="@/assets/img/teeth/31-38/31/31.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/31-38/32/32.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/31-38/33/33.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/31-38/34/34.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/31-38/35/35.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/31-38/36/36.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/31-38/37/37.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/31-38/38/38.png" width="60" alt=""></td>
+                  <td v-for="(item, key) in teeth[3]" :key="key" class="text-center">
+                    <img :src="item.img" width="60" alt="">
+                  </td>
                 </tr>
               </tbody>
               <tfoot>
@@ -110,3 +87,58 @@
     </div>
   </div>
 </template>
+
+<script>
+import { ref } from 'vue'
+export default{
+    name: 'OdontogramaHome',
+    setup(){
+      let teeth = ref([
+        [
+          {id: 18 , img:'/assets/img/teeth/11-18/18/18.png'},
+          {id: 17 , img:'/assets/img/teeth/11-18/17/17.png'},
+          {id: 16 , img:'/assets/img/teeth/11-18/16/16.png'},
+          {id: 15 , img:'/assets/img/teeth/11-18/15/15.png'},
+          {id: 14 , img:'/assets/img/teeth/11-18/14/14.png'},
+          {id: 13 , img:'/assets/img/teeth/11-18/13/13.png'},
+          {id: 12 , img:'/assets/img/teeth/11-18/12/12.png'},
+          {id: 12 , img:'/assets/img/teeth/11-18/11/11.png'},
+        ],
+        [
+          {id: 21 , img:'/assets/img/teeth/21-28/21/21.png'},
+          {id: 22 , img:'/assets/img/teeth/21-28/22/22.png'},
+          {id: 23 , img:'/assets/img/teeth/21-28/23/23.png'},
+          {id: 24 , img:'/assets/img/teeth/21-28/24/24.png'},
+          {id: 25 , img:'/assets/img/teeth/21-28/25/25.png'},
+          {id: 26 , img:'/assets/img/teeth/21-28/26/26.png'},
+          {id: 27 , img:'/assets/img/teeth/21-28/27/27.png'},
+          {id: 28 , img:'/assets/img/teeth/21-28/28/28.png'},
+        ],
+        [
+          {id: 48 , img:'/assets/img/teeth/41-48/48/48.png'},
+          {id: 47 , img:'/assets/img/teeth/41-48/47/47.png'},
+          {id: 46 , img:'/assets/img/teeth/41-48/46/46.png'},
+          {id: 45 , img:'/assets/img/teeth/41-48/45/45.png'},
+          {id: 44 , img:'/assets/img/teeth/41-48/44/44.png'},
+          {id: 43 , img:'/assets/img/teeth/41-48/43/43.png'},
+          {id: 42 , img:'/assets/img/teeth/41-48/42/42.png'},
+          {id: 42 , img:'/assets/img/teeth/41-48/41/41.png'},
+        ],
+        [
+          {id: 31 , img:'/assets/img/teeth/31-38/31/31.png'},
+          {id: 32 , img:'/assets/img/teeth/31-38/32/32.png'},
+          {id: 33 , img:'/assets/img/teeth/31-38/33/33.png'},
+          {id: 34 , img:'/assets/img/teeth/31-38/34/34.png'},
+          {id: 35 , img:'/assets/img/teeth/31-38/35/35.png'},
+          {id: 36 , img:'/assets/img/teeth/31-38/36/36.png'},
+          {id: 37 , img:'/assets/img/teeth/31-38/37/37.png'},
+          {id: 38 , img:'/assets/img/teeth/31-38/38/38.png'},
+        ],
+        
+      ]);
+      return {
+        teeth
+      }
+    }
+  }
+</script>
