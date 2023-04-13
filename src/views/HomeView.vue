8fe04a47e88e5aeeb949b77f9628e550d1ca1f4a
saircode <sairjsanchez@gmail.com>
2023-04-13 11:13:23 -0500

    Avanzando en imagenes y agragando git ignore que se me habia borrado

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -59,25 +59,25 @@
                 </tr>
 
                 <tr>
-                  <td class="text-center"><img src="@/assets/img/teeth/11-18/18/18.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/11-18/17/17.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/11-18/16/16.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/11-18/15/15.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/11-18/14/14.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/11-18/13/13.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/11-18/12/12.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/11-18/11/11.png" width="60" alt=""></td>
+                  <td class="text-center"><img src="@/assets/img/teeth/41-48/48/48.png" width="60" alt=""></td>
+                  <td class="text-center"><img src="@/assets/img/teeth/41-48/47/47.png" width="60" alt=""></td>
+                  <td class="text-center"><img src="@/assets/img/teeth/41-48/46/46.png" width="60" alt=""></td>
+                  <td class="text-center"><img src="@/assets/img/teeth/41-48/45/45.png" width="60" alt=""></td>
+                  <td class="text-center"><img src="@/assets/img/teeth/41-48/44/44.png" width="60" alt=""></td>
+                  <td class="text-center"><img src="@/assets/img/teeth/41-48/43/43.png" width="60" alt=""></td>
+                  <td class="text-center"><img src="@/assets/img/teeth/41-48/42/42.png" width="60" alt=""></td>
+                  <td class="text-center"><img src="@/assets/img/teeth/41-48/41/41.png" width="60" alt=""></td>
 
                   <td class="text-center"></td>
 
-                  <td class="text-center"><img src="@/assets/img/teeth/21-28/21/21.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/21-28/22/22.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/21-28/23/23.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/21-28/24/24.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/21-28/25/25.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/21-28/26/26.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/21-28/27/27.png" width="60" alt=""></td>
-                  <td class="text-center"><img src="@/assets/img/teeth/21-28/28/28.png" width="60" alt=""></td>
+                  <td class="text-center"><img src="@/assets/img/teeth/31-38/31/31.png" width="60" alt=""></td>
+                  <td class="text-center"><img src="@/assets/img/teeth/31-38/32/32.png" width="60" alt=""></td>
+                  <td class="text-center"><img src="@/assets/img/teeth/31-38/33/33.png" width="60" alt=""></td>
+                  <td class="text-center"><img src="@/assets/img/teeth/31-38/34/34.png" width="60" alt=""></td>
+                  <td class="text-center"><img src="@/assets/img/teeth/31-38/35/35.png" width="60" alt=""></td>
+                  <td class="text-center"><img src="@/assets/img/teeth/31-38/36/36.png" width="60" alt=""></td>
+                  <td class="text-center"><img src="@/assets/img/teeth/31-38/37/37.png" width="60" alt=""></td>
+                  <td class="text-center"><img src="@/assets/img/teeth/31-38/38/38.png" width="60" alt=""></td>
                 </tr>
               </tbody>
               <tfoot>
